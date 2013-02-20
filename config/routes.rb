21c9a9824aa8e5a9847a38cf7b92b24552fa20f7
TKVR <mike@michaellungo.com>
2013-02-20 08:54:32 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -85,6 +85,11 @@ Teamzoom::Application.routes.draw do
         put :change_status
       end
     end
+    resources :request_installs do
+      member do
+        get :index
+      end
+    end
     resources :teams do
       collection do
         get :search
@@ -200,7 +205,7 @@ Teamzoom::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
 
 
 end
