9fb5c9602bd74e71a9ce390e47bc6b2118a9c2ab
rpruiz <rpruiz@gmail.com>
2013-03-22 19:36:52 -0600

    New deploy.rb, still not working at 100%

--- config/deploy_new.rb
+++ config/deploy_new.rb
@@ -1,81 +1,76 @@
-# $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-
-# Load rvm's capistrono plugins
 require 'rvm/capistrano'
-
 require 'bundler/capistrano'
 
 set :rvm_type, :user
 
-set :user, 'ubuntu'
+set :application, "54.243.30.244"
 set :domain, '54.243.30.244'
-set :application, "teamzoom_pro"
-set :keep_releases, 2 # It keeps on two old releases.
 
-# git repo details
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-set :repository,  "git@github.com:TKVR/teamzoom.git"
-set :scm_username, 'TKVR'
-set :git_enable_submodules, 1
+# Roles
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+#deployment details
+set :deploy_via, :remote_cache
+set :user, "ubuntu"
+set :copy_compression, :bz2
 set :git_shallow_clone, 1
-set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :deploy_to, "/home/ubuntu/teamzoom_pro_set2"
 
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-
-role :web, domain                          # Your HTTP server, Apache/etc
-role :app, domain                          # This may be the same as your `Web` server
-role :db,  domain, :primary => true# 'ec2-23-23-156-118.compute-1.amazonaws.com' This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# deply options
 default_run_options[:pty] = true
 set :ssh_options, {:forward_agent => true}
 set :ssh_options, {:auth_methods => "publickey"}
 set :ssh_options, {:keys => ["~/Downloads/teamzoom-1.pem"]}
-set :deploy_to, "/home/ubuntu/teamzoom_pro_set2"
-#set :deploy_via, :remote_cache
-set :use_sudo, false
 
-# if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
+#repo details
+set :scm, :git
+set :repository,  "git@github.com:TKVR/teamzoom.git"
+set :scm_username, 'TKVR'
+set :keep_releases, 2
+set :branch, "master"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-  task :start do
-    # run COMMAND="/etc/init.d/nginx restart" invoke SUDO=1
-    run "sudo /etc/init.d/nginx restart"
-    # exit
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "service nginx start"
+    run "cd #{release_path} && touch tmp/restart.txt"
   end
+
   after "deploy:start", "deploy:cleanup"
 
-  task :stop do ; end
+  after 'deploy:cleanup', 'deploy:symlink_config'
+
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "service nginx stop"
+  end
+
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "service nginx stop"
+  end
+
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "cd #{release_path} && touch tmp/restart.txt"
+    run "service nginx restart"
+  end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-
-  task :setup_config, roles: :app do
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts 'now edit the config file database in #{shared_path}'
-  end
-  after 'deploy:setup', 'deploy:setup_config'
-
-  desc "Symlink shared resources on each release - not used"
-  task :symlink_config, :roles => :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    stop
+    start
   end
   
-  after 'deploy:finalize_update', 'deploy:symlink_config'
-
-  desc "It helps to seed database with values"
-  task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-  end
-  task :create_schema do
-    run "cd #{current_path}; bundle exec rake db:create RAILS_ENV=#{rails_env} --trace"
+  task :pipeline_precompile do
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
+    # precompile assets before deploy and upload them to server 
+    # run_locally("RAILS_ENV=#{rails_env} rake assets:clean && RAILS_ENV=#{rails_env} rake assets:precompile")
+    # top.upload "public/assets", "#{release_path}/public/assets", :via =>:scp, :recursive => true
   end
 end
+
+before "deploy:assets:precompile", "bundle:install"
+
+# remove old releases
+after "deploy", "deploy:cleanup"
+
+after "deploy:update_code", "deploy:pipeline_precompile"
